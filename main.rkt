5bec2aec463fe39edd271a38974fca9e4cdf19b6
Hanwen Guo <guo@hanwen.io>
2024-12-04 15:57:54 -0700

    code: update main.rkt to produce more elegant output

--- main.rkt
+++ main.rkt
@@ -4,6 +4,7 @@
 (require "lib.rkt")
 
 (define benchmark-output-format (make-parameter 'plain))
+(define benchmark-output-transposed (make-parameter #f)) ; one type checker per row by default, set to #t to have one type checker per column
 
 (define typechecker-parameters-alist
   `((typedracket (comment-char #\;)
@@ -65,7 +66,61 @@
     [(tex) (print-benchmark-row-latex type-checker benchmark-result)]
     [(plain) (display (format "~a: ~a\n" type-checker benchmark-result))]))
 
-(define (run-benchmark type-checker)
+(define (process-benchmark-row type-checker benchmark-result-row)
+  (append (list (symbol->string type-checker))
+          (map (lambda (test)
+                 (let ([positive-passed (cadr test)]
+                       [negative-passed (caddr test)])
+                   (cond
+                     [(and positive-passed negative-passed) "V"]
+                     [(or positive-passed negative-passed) "x"]
+                     [else "X"])))
+               benchmark-result-row)))
+
+(define (print-row-markdown row)
+  (display (format "| ~a " (car row)))
+  (for-each (lambda (test-result)
+              (display (format "| ~a " test-result)))
+            (cdr row))
+  (display "|\n"))
+
+(define (print-row-latex row)
+  (display (format "~a " (car row)))
+  (for-each (lambda (test-result)
+              (display (format "& ~a " test-result)))
+            (cdr row))
+  (display "\\\\\n"))
+
+(define (print-row row [output-format 'plain])
+  (case output-format
+    [(markdown) (print-row-markdown row)]
+    [(tex) (print-row-latex row)]
+    [(plain) (display (format "~a\n" row))]))
+
+(define (transpose l)
+  (apply map list l))
+
+(define (pad-columns rows)
+  (define transposed-rows (transpose rows))
+  (define column-widths (map (lambda (column) (apply max (map string-length column))) transposed-rows))
+  (define padded-rows
+    (map (lambda (row)
+           (map (lambda (column width)
+                  (format "~a~a" column (make-string (- width (string-length column)) #\space)))
+                row column-widths))
+         rows))
+  padded-rows)
+
+(define (print-benchmark benchmark-result-rows [output-format 'plain] [table-header null])
+  (define transpose-function (if (benchmark-output-transposed) transpose identity))
+  (define processed-benchmark-result-rows (map (lambda (row) (process-benchmark-row (car row) (cdr row))) benchmark-result-rows))
+  (define printable-rows-pre (transpose (if (not (null? table-header))
+                                            (cons table-header processed-benchmark-result-rows)
+                                            processed-benchmark-result-rows)))
+  (define printable-rows (pad-columns printable-rows-pre))
+  (for-each (lambda (row) (print-row row output-format)) printable-rows))
+
+(define (get-benchmark-result-row type-checker)
   (when (benchmark-verbose)
     (displayln (format "Running benchmark for ~a" type-checker)))
   (define typechecker-parameters (cdr (assoc type-checker typechecker-parameters-alist)))
@@ -86,10 +141,25 @@
                               #:post-benchmark-func post-benchmark-func
                               #:pre-benchmark-func-dir pre-benchmark-func-dir
                               #:post-benchmark-func-dir post-benchmark-func-dir))))
-  (print-benchmark-row type-checker benchmark-result))
+  (cons type-checker benchmark-result))
 
-(define (run-all-benchmarks)
-  (for-each run-benchmark (map car typechecker-parameters-alist)))
+(define (run-benchmarks type-checker-list)
+  (define benchmark-result-rows
+    (map get-benchmark-result-row type-checker-list))
+  (print-benchmark benchmark-result-rows (benchmark-output-format)
+                   (cons "Benchmark" '("positive"
+                                       "negative"
+                                       "alias"
+                                       "connectives"
+                                       "nesting_body"
+                                       "nesting_condition"
+                                       "predicate_2way"
+                                       "predicate_1way"
+                                       "predicate_checked"
+                                       "object_properties"
+                                       "tuple_elements"
+                                       "tuple_length"
+                                       "merge_with_union"))))
 
 (define type-checker
   (command-line
@@ -99,6 +169,8 @@
                        (benchmark-verbose #t)]
    [("-f" "--format") output-format "Print the output of the benchmarks in the specified format. Options: plain, markdown, tex. Default: plain."
                       (benchmark-output-format (string->symbol output-format))]
+   [("-t" "--transpose") "Transpose the output of the benchmarks"
+                         (benchmark-output-transposed #t)]
    ;; #:once-any
    ;; [("-m" "--markdown") "Print the output of the benchmarks in markdown format"
    ;;                      (benchmark-output-format 'markdown)]
@@ -108,5 +180,5 @@
    type-checker))
 
 (if (null? type-checker)
-    (run-all-benchmarks)
-    (run-benchmark (string->symbol (string-downcase type-checker))))
+    (run-benchmarks (map car typechecker-parameters-alist))
+    (run-benchmarks (list (string->symbol (string-downcase type-checker)))))
