8902dfebcc4b46c28bb7e6a4b713884d88305f20
sivasathyaseeelan <dnsiva.sathyaseelan.chy21@iitbhu.ac.in>
2025-06-03 18:29:29 +0530

    changes as per review

--- Sorbet/main.rb
+++ Sorbet/main.rb
@@ -1,26 +1,30 @@
 # typed: strict
 extend T::Sig
 
-# Example positive
-sig { params(x: T.untyped).returns(T.untyped) }
+### Code:
+## Example positive
+## success
+sig { params(x: T.untyped).returns(Integer) }
 def positive_success_f(x)
   if x.is_a?(String)
     x.length
   else
-    x
+    0
   end
 end
 
-sig { params(x: T.untyped).returns(T.untyped) }
+## failure
+sig { params(x: T.untyped).returns(Integer) }
 def positive_failure_f(x)
   if x.is_a?(String)
     x.is_nan # Expected error: No method 'is_nan' on String
   else
-    x
+    0
   end
 end
 
-# Example negative
+## Example negative
+## success
 sig { params(x: T.any(String, Integer)).returns(Integer) }
 def negative_success_f(x)
   if x.is_a?(String)
@@ -30,6 +34,7 @@ def negative_success_f(x)
   end
 end
 
+## failure
 sig { params(x: T.any(String, Integer, TrueClass, FalseClass)).returns(Integer) }
 def negative_failure_f(x)
   if x.is_a?(String)
@@ -39,39 +44,42 @@ def negative_failure_f(x)
   end
 end
 
-# Example alias
-sig { params(x: T.untyped).returns(T.untyped) }
+## Example alias
+## success
+sig { params(x: T.untyped).returns(Integer) }
 def alias_success_f(x)
   y = x.is_a?(String)
   if y
     x.length
   else
-    x
+    0
   end
 end
 
-sig { params(x: T.untyped).returns(T.untyped) }
+## failure
+sig { params(x: T.untyped).returns(Integer) }
 def alias_failure_f(x)
   y = x.is_a?(String)
   if y
     x.is_nan # Expected error: No method 'is_nan' on String
   else
-    x
+    0
   end
 end
 
-sig { params(x: T.untyped).returns(T.untyped) }
+## failure
+sig { params(x: T.any(String, Integer)).returns(Integer) }
 def alias_failure_g(x)
   y = x.is_a?(String)
-  y = true
   if y
-    x.length # Expected error: x not guaranteed to be String
+    x.length
   else
-    x
+    x.length # Expected error: length not defined for Integer
   end
 end
 
-# Example connectives
+## Example connectives
+## success
 sig { params(x: T.any(String, Integer)).returns(Integer) }
 def connectives_success_f(x)
   if !x.is_a?(Integer)
@@ -81,6 +89,7 @@ def connectives_success_f(x)
   end
 end
 
+## success
 sig { params(x: T.untyped).returns(Integer) }
 def connectives_success_g(x)
   if x.is_a?(String) || x.is_a?(Integer)
@@ -90,15 +99,17 @@ def connectives_success_g(x)
   end
 end
 
+## success
 sig { params(x: T.any(String, Integer, TrueClass, FalseClass)).returns(Integer) }
 def connectives_success_h(x)
-  if x.is_a?(String)
+  if !x.is_a?(TrueClass) && !x.is_a?(FalseClass) && !x.is_a?(Integer)
     x.length
   else
     0
   end
 end
 
+## failure
 sig { params(x: T.any(String, Integer)).returns(Integer) }
 def connectives_failure_f(x)
   if !x.is_a?(Integer)
@@ -108,6 +119,7 @@ def connectives_failure_f(x)
   end
 end
 
+## failure
 sig { params(x: T.untyped).returns(Integer) }
 def connectives_failure_g(x)
   if x.is_a?(String) || x.is_a?(Integer)
@@ -117,35 +129,47 @@ def connectives_failure_g(x)
   end
 end
 
+## failure
 sig { params(x: T.any(String, Integer, TrueClass, FalseClass)).returns(Integer) }
 def connectives_failure_h(x)
-  if x.is_a?(String)
-    x.length
-  else
-    x.length # Expected error: length not defined for Integer | TrueClass | FalseClass
-  end
-end
-
-# Example nesting_body
-sig { params(x: T.any(String, Integer, TrueClass, FalseClass)).returns(Integer) }
-def nesting_body_success_f(x)
-  if x.is_a?(Integer)
-    x + 1
+  if !x.is_a?(TrueClass) && !x.is_a?(FalseClass) && !x.is_a?(Integer)
+    x.is_nan # Expected error: is_nan not defined for String
   else
     0
   end
 end
 
+## Example nesting_body
+## success
+sig { params(x: T.any(String, Integer, TrueClass, FalseClass)).returns(Integer) }
+def nesting_body_success_f(x)
+  if !x.is_a?(String)
+    if !x.is_a?(TrueClass) && !x.is_a?(FalseClass)
+      x + 1
+    else
+      0
+    end
+  else
+    0
+  end
+end
+
+## failure
 sig { params(x: T.any(String, Integer, TrueClass, FalseClass)).returns(Integer) }
 def nesting_body_failure_f(x)
   if x.is_a?(String) || x.is_a?(Integer)
-    x.length # Expected error: length not defined for Integer
+    if x.is_a?(Integer)
+      x.length # Expected error: length not defined for Integer
+    else
+      0
+    end
   else
     0
   end
 end
 
-# Example nesting_condition
+## Example nesting_condition
+## success
 sig { params(x: T.untyped, y: T.untyped).returns(Integer) }
 def nesting_condition_success_f(x, y)
   if x.is_a?(Integer) ? y.is_a?(String) : false
@@ -155,6 +179,7 @@ def nesting_condition_success_f(x, y)
   end
 end
 
+## failure
 sig { params(x: T.any(String, Integer), y: T.untyped).returns(Integer) }
 def nesting_condition_failure_f(x, y)
   if x.is_a?(Integer) ? y.is_a?(String) : y.is_a?(String)
@@ -164,86 +189,83 @@ def nesting_condition_failure_f(x, y)
   end
 end
 
-# Example predicate_2way
+## Example predicate_2way
+## success
 sig { params(x: T.any(String, Integer)).returns(T::Boolean) }
 def predicate_2way_success_f(x)
-  x.is_a?(String)
+  true # Dummy return, Sorbet lacks type predicates
 end
 
+## success
 sig { params(x: T.any(String, Integer)).returns(Integer) }
 def predicate_2way_success_g(x)
-  if predicate_2way_success_f(x)
-    x.length
-  else
-    x + 0
-  end
+  0 # Dummy return, Sorbet lacks type predicates
 end
 
+## failure
 sig { params(x: T.any(String, Integer)).returns(T::Boolean) }
 def predicate_2way_failure_f(x)
-  x.is_a?(String)
+  x.is_nan # Expected error: is_nan not defined for String | Integer
 end
 
+## failure
 sig { params(x: T.any(String, Integer)).returns(Integer) }
 def predicate_2way_failure_g(x)
-  if predicate_2way_failure_f(x)
-    x.is_nan # Expected error: is_nan not defined for String
-  else
-    x + 0
-  end
+  x.length # Expected error: length not defined for Integer
 end
 
-# Example predicate_1way
+## Example predicate_1way
+## success
 sig { params(x: T.any(String, Integer)).returns(T::Boolean) }
 def predicate_1way_success_f(x)
-  x.is_a?(Integer) && x > 0
+  true # Dummy return, Sorbet lacks type predicates
 end
 
+## success
 sig { params(x: T.any(String, Integer)).returns(Integer) }
 def predicate_1way_success_g(x)
-  if predicate_1way_success_f(x)
-    x + 1
-  else
-    0
-  end
+  0 # Dummy return, Sorbet lacks type predicates
 end
 
+## failure
 sig { params(x: T.any(String, Integer)).returns(T::Boolean) }
 def predicate_1way_failure_f(x)
-  x.is_a?(Integer) && x > 0
+  x.is_nan # Expected error: is_nan not defined for String | Integer
 end
 
+## failure
 sig { params(x: T.any(String, Integer)).returns(Integer) }
 def predicate_1way_failure_g(x)
-  if predicate_1way_failure_f(x)
-    x + 1
-  else
-    x.length # Expected error: length not defined for String | Integer
-  end
+  x.length # Expected error: length not defined for Integer
 end
 
-# Example predicate_checked
+## Example predicate_checked
+## success
 sig { params(x: T.any(String, Integer, TrueClass, FalseClass)).returns(T::Boolean) }
 def predicate_checked_success_f(x)
-  x.is_a?(String)
+  true # Dummy return, Sorbet lacks type predicates
 end
 
+## success
 sig { params(x: T.any(String, Integer, TrueClass, FalseClass)).returns(T::Boolean) }
 def predicate_checked_success_g(x)
-  !predicate_checked_success_f(x)
+  true # Dummy return, Sorbet lacks type predicates
 end
 
+## failure
 sig { params(x: T.any(String, Integer, TrueClass, FalseClass)).returns(T::Boolean) }
 def predicate_checked_failure_f(x)
-  x.is_a?(String) || x.is_a?(Integer)
+  x.is_nan # Expected error: is_nan not defined for String | Integer | TrueClass | FalseClass
 end
 
+## failure
 sig { params(x: T.any(String, Integer, TrueClass, FalseClass)).returns(T::Boolean) }
 def predicate_checked_failure_g(x)
-  x.is_a?(Integer)
+  x.length # Expected error: length not defined for Integer | TrueClass | FalseClass
 end
 
-# Example object_properties
+## Example object_properties
+## success
 sig { params(x: { a: T.untyped }).returns(Integer) }
 def object_properties_success_f(x)
   if x[:a].is_a?(Integer)
@@ -253,16 +275,18 @@ def object_properties_success_f(x)
   end
 end
 
-sig { params(x: { a: T.untyped }).returns(Integer) }
+## failure
+sig { params(x: { a: T.any(String, Integer) }).returns(Integer) }
 def object_properties_failure_f(x)
   if x[:a].is_a?(String)
-    x[:a] # Expected error: String not assignable to Integer
+    x[:a].length # Expected error: String#length not assignable to Integer
   else
-    0
+    x[:a]
   end
 end
 
-# Example tuple_elements
+## Example tuple_elements
+## success
 sig { params(x: [T.untyped, T.untyped]).returns(Integer) }
 def tuple_elements_success_f(x)
   if x[0].is_a?(Integer)
@@ -272,16 +296,18 @@ def tuple_elements_success_f(x)
   end
 end
 
-sig { params(x: [T.untyped, T.untyped]).returns(Integer) }
+## failure
+sig { params(x: [Integer, T.any(String, Integer)]).returns(Integer) }
 def tuple_elements_failure_f(x)
   if x[0].is_a?(Integer)
-    x[1].length # Expected error: length not defined for T.untyped
+    x[1].length # Expected error: length not defined for Integer
   else
     0
   end
 end
 
-# Example tuple_length
+## Example tuple_length
+## success
 sig { params(x: T.any([Integer, Integer], [String, String, String])).returns(Integer) }
 def tuple_length_success_f(x)
   if x.length == 2
@@ -291,6 +317,7 @@ def tuple_length_success_f(x)
   end
 end
 
+## failure
 sig { params(x: T.any([Integer, Integer], [String, String, String])).returns(Integer) }
 def tuple_length_failure_f(x)
   if x.length == 2
@@ -300,7 +327,8 @@ def tuple_length_failure_f(x)
   end
 end
 
-# Example merge_with_union
+## Example merge_with_union
+## success
 sig { params(x: T.untyped).returns(T.any(String, Integer)) }
 def merge_with_union_success_f(x)
   if x.is_a?(String)
@@ -313,6 +341,7 @@ def merge_with_union_success_f(x)
   x
 end
 
+## failure
 sig { params(x: T.untyped).returns(T.any(String, Integer)) }
 def merge_with_union_failure_f(x)
   if x.is_a?(String)
