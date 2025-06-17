8e457729efec984355a37b91132d8c289f477efc
Siva Sathyaseelan D N <95441117+sivasathyaseeelan@users.noreply.github.com>
2025-06-18 00:05:56 +0530

    Sorbet (#20)
    * Sorbet added
    
    * fixed mistake
    
    * changes as per review
    
    * readme aadded
    
    * added example.rb
    
    * added example.rb
    
    * added raise in predicate
    
    * some changes
    
    * added Sorbet to main.rkt
    
    * _benchmark.rkt is added
    
    * setup.md updated
    
    * some changes
    
    * some changes
    
    * bug fixed
    
    * readme updated
    
    * updated README
    
    * some changes
    
    * some changes
    
    * example.rb fixed

---
+++ Sorbet/examples.rb
@@ -0,0 +1,139 @@
+# typed: strict
+extend T::Sig
+
+### Code:
+## Example filter
+## success
+sig { params(array: T::Array[T.untyped], callbackfn: T.proc.params(value: T.untyped).returns(T::Boolean)).returns(T::Array[T.untyped]) }
+def filter_success(array, callbackfn)
+  result = T.let([], T::Array[T.untyped])
+  array.each do |value|
+    if callbackfn.call(value)
+      result << value
+    end
+  end
+  result
+end
+
+## failure
+sig { params(array: T::Array[T.untyped], callbackfn: T.proc.params(value: T.untyped).returns(T::Boolean)).returns(T::Array[Integer]) }
+def filter_failure(array, callbackfn)
+  result = T.let([], T::Array[Integer])
+  array.each do |value|
+    if callbackfn.call(value)
+      result << T.cast(value, Integer)
+    else
+      result << "string" # Expected error: Cannot append String to T::Array[Integer]
+    end
+  end
+  result
+end
+
+## Example flatten
+## success
+sig { params(l: T.untyped).returns(T::Array[Integer]) }
+def flatten_success(l)
+  if l.is_a?(Array)
+    if l.length == 0
+      T.let([], T::Array[Integer])
+    else
+      first = T.let(flatten_success(l[0]), T::Array[Integer])
+      rest = T.let(flatten_success(l[1..-1] || []), T::Array[Integer])
+      T.let(first.concat(rest), T::Array[Integer])
+    end
+  else
+    T.let([T.cast(l, Integer)], T::Array[Integer])
+  end
+end
+
+## failure
+sig { params(l: T.untyped).returns(T::Array[Integer]) }
+def flatten_failure(l)
+  if l.is_a?(Array)
+    if l.length == 0
+      T.let([], T::Array[Integer])
+    else
+      first = T.let(flatten_failure(l[0]), T::Array[Integer])
+      rest = T.let(flatten_failure(l[1..-1] || []), T::Array[Integer])
+      T.let(first.concat(rest), T::Array[Integer])
+    end
+  else
+    T.let(l, Integer) # Expected error: Expected T::Array[Integer] but found Integer
+  end
+end
+
+## Example tree_node
+## success
+class TreeNodeSuccess
+  extend T::Sig
+  sig { returns(Integer) }
+  attr_reader :value
+  sig { returns(T.nilable(T::Array[TreeNodeSuccess])) }
+  attr_reader :children
+
+  sig { params(value: Integer, children: T.nilable(T::Array[TreeNodeSuccess])).void }
+  def initialize(value:, children: nil)
+    raise "Sorbet does not support type predicates"
+  end
+
+  sig { params(node: T.untyped).returns(T::Boolean) }
+  def self.is_tree_node_success(node)
+    raise "Sorbet does not support type predicates"
+  end
+end
+
+## failure
+class TreeNodeFailure
+  extend T::Sig
+  sig { returns(Integer) }
+  attr_reader :value
+  sig { returns(T.nilable(T::Array[TreeNodeFailure])) }
+  attr_reader :children
+
+  sig { params(value: Integer, children: T.nilable(T::Array[TreeNodeFailure])).void }
+  def initialize(value:, children: nil)
+    raise "Sorbet does not support type predicates"
+  end
+
+  sig { params(node: T.untyped).returns(T::Boolean) }
+  def self.is_tree_node_failure(node)
+    raise "Sorbet does not support type predicates"
+  end
+end
+
+## Example rainfall
+## success
+sig { params(weather_reports: T::Array[T.untyped]).returns(Float) }
+def rainfall_success(weather_reports)
+  total = T.let(0.0, Float)
+  count = T.let(0, Integer)
+  weather_reports.each do |day|
+    if day.is_a?(T::Hash[Symbol, T.untyped]) && !day.nil?
+      if day.key?(:rainfall)
+        val = T.let(day[:rainfall], T.untyped)
+        if val.is_a?(Float) && 0.0 <= val && val <= 999.0
+          total += val
+          count += 1
+        end
+      end
+    end
+  end
+  count > 0 ? total / count : 0.0
+end
+
+## failure
+sig { params(weather_reports: T::Array[T.untyped]).returns(Float) }
+def rainfall_failure(weather_reports)
+  total = T.let(0.0, Float)
+  count = T.let(0, Integer)
+  weather_reports.each do |day|
+    if day.is_a?(T::Hash[Symbol, T.untyped]) && !day.nil?
+      if day.key?(:rainfall)
+        val = T.cast(day[:rainfall], String)
+        total += val # Expected error: Expected Integer but found String
+        count += 1
+      end
+    end
+  end
+  count > 0 ? total / count : 0.0
+end
---
+++ Sorbet/main.rb
@@ -0,0 +1,344 @@
+# typed: strict
+extend T::Sig
+
+### Code:
+## Example positive
+## success
+sig { params(x: T.untyped).returns(Integer) }
+def positive_success_f(x)
+  if x.is_a?(String)
+    x.length
+  else
+    0
+  end
+end
+
+## failure
+sig { params(x: T.untyped).returns(Integer) }
+def positive_failure_f(x)
+  if x.is_a?(String)
+    x.is_nan # Expected error: No method 'is_nan' on String
+  else
+    0
+  end
+end
+
+## Example negative
+## success
+sig { params(x: T.any(String, Integer)).returns(Integer) }
+def negative_success_f(x)
+  if x.is_a?(String)
+    x.length
+  else
+    x + 1
+  end
+end
+
+## failure
+sig { params(x: T.any(String, Integer, TrueClass, FalseClass)).returns(Integer) }
+def negative_failure_f(x)
+  if x.is_a?(String)
+    x.length
+  else
+    x + 1 # Expected error: + not defined for TrueClass | FalseClass
+  end
+end
+
+## Example alias
+## success
+sig { params(x: T.untyped).returns(Integer) }
+def alias_success_f(x)
+  y = x.is_a?(String)
+  if y
+    x.length
+  else
+    0
+  end
+end
+
+## failure
+sig { params(x: T.untyped).returns(Integer) }
+def alias_failure_f(x)
+  y = x.is_a?(String)
+  if y
+    x.is_nan # Expected error: No method 'is_nan' on String
+  else
+    0
+  end
+end
+
+sig { params(x: T.any(String, Integer)).returns(Integer) }
+def alias_failure_g(x)
+  y = x.is_a?(String)
+  if y
+    x.length
+  else
+    x.length # Expected error: length not defined for Integer
+  end
+end
+
+## Example connectives
+## success
+sig { params(x: T.any(String, Integer)).returns(Integer) }
+def connectives_success_f(x)
+  if !x.is_a?(Integer)
+    x.length
+  else
+    0
+  end
+end
+
+sig { params(x: T.untyped).returns(Integer) }
+def connectives_success_g(x)
+  if x.is_a?(String) || x.is_a?(Integer)
+    connectives_success_f(x)
+  else
+    0
+  end
+end
+
+sig { params(x: T.any(String, Integer, TrueClass, FalseClass)).returns(Integer) }
+def connectives_success_h(x)
+  if !x.is_a?(TrueClass) && !x.is_a?(FalseClass) && !x.is_a?(Integer)
+    x.length
+  else
+    0
+  end
+end
+
+## failure
+sig { params(x: T.any(String, Integer)).returns(Integer) }
+def connectives_failure_f(x)
+  if !x.is_a?(Integer)
+    x.is_nan # Expected error: is_nan not defined for String
+  else
+    0
+  end
+end
+
+sig { params(x: T.untyped).returns(Integer) }
+def connectives_failure_g(x)
+  if x.is_a?(String) || x.is_a?(Integer)
+    x.length # Expected error: length not defined for String | Integer
+  else
+    0
+  end
+end
+
+sig { params(x: T.any(String, Integer, TrueClass, FalseClass)).returns(Integer) }
+def connectives_failure_h(x)
+  if !x.is_a?(TrueClass) && !x.is_a?(FalseClass) && !x.is_a?(Integer)
+    x.is_nan # Expected error: is_nan not defined for String
+  else
+    0
+  end
+end
+
+## Example nesting_body
+## success
+sig { params(x: T.any(String, Integer, TrueClass, FalseClass)).returns(Integer) }
+def nesting_body_success_f(x)
+  if !x.is_a?(String)
+    if !x.is_a?(TrueClass) && !x.is_a?(FalseClass)
+      x + 1
+    else
+      0
+    end
+  else
+    0
+  end
+end
+
+## failure
+sig { params(x: T.any(String, Integer, TrueClass, FalseClass)).returns(Integer) }
+def nesting_body_failure_f(x)
+  if x.is_a?(String) || x.is_a?(Integer)
+    if x.is_a?(Integer)
+      x.length # Expected error: length not defined for Integer
+    else
+      0
+    end
+  else
+    0
+  end
+end
+
+## Example nesting_condition
+## success
+sig { params(x: T.untyped, y: T.untyped).returns(Integer) }
+def nesting_condition_success_f(x, y)
+  if x.is_a?(Integer) ? y.is_a?(String) : false
+    x + y.length
+  else
+    0
+  end
+end
+
+## failure
+sig { params(x: T.any(String, Integer), y: T.untyped).returns(Integer) }
+def nesting_condition_failure_f(x, y)
+  if x.is_a?(Integer) ? y.is_a?(String) : y.is_a?(String)
+    x.length # Expected error: length not defined for String | Integer
+  else
+    0
+  end
+end
+
+## Example predicate_2way
+## success
+sig { params(x: T.any(String, Integer)).returns(T::Boolean) }
+def predicate_2way_success_f(x)
+  raise "Sorbet does not support type predicates"
+end
+
+sig { params(x: T.any(String, Integer)).returns(Integer) }
+def predicate_2way_success_g(x)
+  raise "Sorbet does not support type predicates"
+end
+
+## failure
+sig { params(x: T.any(String, Integer)).returns(T::Boolean) }
+def predicate_2way_failure_f(x)
+  raise "Sorbet does not support type predicates"
+end
+
+sig { params(x: T.any(String, Integer)).returns(Integer) }
+def predicate_2way_failure_g(x)
+  raise "Sorbet does not support type predicates"
+end
+
+## Example predicate_1way
+## success
+sig { params(x: T.any(String, Integer)).returns(T::Boolean) }
+def predicate_1way_success_f(x)
+  raise "Sorbet does not support type predicates"
+end
+
+sig { params(x: T.any(String, Integer)).returns(Integer) }
+def predicate_1way_success_g(x)
+  raise "Sorbet does not support type predicates"
+end
+
+## failure
+sig { params(x: T.any(String, Integer)).returns(T::Boolean) }
+def predicate_1way_failure_f(x)
+  raise "Sorbet does not support type predicates"
+end
+
+sig { params(x: T.any(String, Integer)).returns(Integer) }
+def predicate_1way_failure_g(x)
+  raise "Sorbet does not support type predicates"
+end
+
+## Example predicate_checked
+## success
+sig { params(x: T.any(String, Integer, TrueClass, FalseClass)).returns(T::Boolean) }
+def predicate_checked_success_f(x)
+  raise "Sorbet does not support type predicates"
+end
+
+sig { params(x: T.any(String, Integer, TrueClass, FalseClass)).returns(T::Boolean) }
+def predicate_checked_success_g(x)
+  raise "Sorbet does not support type predicates"
+end
+
+## failure
+sig { params(x: T.any(String, Integer, TrueClass, FalseClass)).returns(T::Boolean) }
+def predicate_checked_failure_f(x)
+  raise "Sorbet does not support type predicates"
+end
+
+sig { params(x: T.any(String, Integer, TrueClass, FalseClass)).returns(T::Boolean) }
+def predicate_checked_failure_g(x)
+  raise "Sorbet does not support type predicates"
+end
+
+## Example struct_fields
+## success
+sig { params(x: { a: T.untyped }).returns(Integer) }
+def struct_fields_success_f(x)
+  if x[:a].is_a?(Integer)
+    x[:a]
+  else
+    0
+  end
+end
+
+## failure
+sig { params(x: { a: T.any(String, Integer) }).returns(Integer) }
+def struct_fields_failure_f(x)
+  if x[:a].is_a?(String)
+    x[:a] # Expected error: Expected Integer but found String
+  else
+    0
+  end
+end
+
+## Example tuple_elements
+## success
+sig { params(x: [T.untyped, T.untyped]).returns(Integer) }
+def tuple_elements_success_f(x)
+  if x[0].is_a?(Integer)
+    x[0]
+  else
+    0
+  end
+end
+
+## failure
+sig { params(x: [Integer, T.any(String, Integer)]).returns(Integer) }
+def tuple_elements_failure_f(x)
+  if x[0].is_a?(Integer)
+    x[1].length # Expected error: length not defined for Integer
+  else
+    0
+  end
+end
+
+## Example tuple_length
+## success
+sig { params(x: T.any([Integer, Integer], [String, String, String])).returns(Integer) }
+def tuple_length_success_f(x)
+  if x.length == 2
+    x[0] + x[1]
+  else
+    x[0].length
+  end
+end
+
+## failure
+sig { params(x: T.any([Integer, Integer], [String, String, String])).returns(Integer) }
+def tuple_length_failure_f(x)
+  if x.length == 2
+    x[0] + x[1]
+  else
+    T.let(x[0].length, Integer) # Expected error: Cannot assign String#length to Integer
+  end
+end
+
+## Example merge_with_union
+## success
+sig { params(x: T.untyped).returns(T.any(String, Integer)) }
+def merge_with_union_success_f(x)
+  if x.is_a?(String)
+    x += "hello"
+  elsif x.is_a?(Integer)
+    x += 1
+  else
+    return 0
+  end
+  x
+end
+
+## failure
+sig { params(x: T.untyped).returns(T.any(String, Integer)) }
+def merge_with_union_failure_f(x)
+  if x.is_a?(String)
+    x += "hello"
+  elsif x.is_a?(Integer)
+    x += 1
+  else
+    return 0
+  end
+  x.is_nan # Expected error: No method 'is_nan' on String | Integer
+end
